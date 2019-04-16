d136e9f44a5c936e14d38f2d5d88971229d11f26
Alexandre <alexandre.fava@hotmail.com>
2019-04-16 11:44:31 -0300

    Envio do Arquivo PHP ConsultaAlunoDesempenhoAcerto

---
+++ Professor/Files/InfanciaSegura-ConsultaAlunoDesempenhoAcerto.php
@@ -0,0 +1,32 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+
+    $servidor = "";
+    $usuario = "";
+    $senha = "";
+    $banco = "";
+
+    $conexao = new mysqli($servidor, $usuario, $senha, $banco) or die("Erro 500. Falha ao se conectar.");
+    mysqli_set_charset($conexao,"utf8");
+
+    $turma = $_GET['turma'];
+    $aluno = $_GET['aluno'];
+
+    $consultaSQL = "SELECT alu.Id FROM Aluno alu INNER JOIN Turma tur ON tur.Id=alu.Turma WHERE tur.Nome='$turma' AND alu.Nome='$aluno'";
+    $resultado=mysqli_query($conexao, $consultaSQL) or die("Erro 404. Falha ao realizar a consulta SQL.");
+    ini_set ('default_charset', 'UTF8');
+
+    $IdAluno = mysqli_fetch_array($resultado);
+
+    $consultaSQL = "SELECT * FROM Desempenho WHERE Aluno='$IdAluno[0]'AND Tipo='Acerto'";
+    $resultado=mysqli_query($conexao, $consultaSQL) or die("Erro 404. Falha ao realizar a consulta SQL.");
+    ini_set ('default_charset', 'UTF8');
+
+    while($dados = mysqli_fetch_array($resultado)){
+        echo $dados['Fase'] . " (Dificuldade: ";
+        echo $dados['Dificuldade'] . ") - Pontos: ";
+        echo $dados['Pontuacao'] . "<br>";
+    }
+
+    mysqli_close($conexao);
+?>
