f1a43f9637ffe21f51a1467026168a44a5780e54
Alexandre <alexandre.fava@hotmail.com>
2019-06-05 11:25:27 -0300

    Envio do Arquivo PHP ConsultarTodasTurmasVisiveis

---
+++ Aluno/Files/InfanciaSegura-ConsultaTodasTurmasVisiveisID.php
@@ -0,0 +1,23 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+
+    $servidor = "";
+    $usuario = "";
+    $senha = "";
+    $banco = "";
+
+    $conexao = new mysqli($servidor, $usuario, $senha, $banco) or die("Erro 500. Falha ao se conectar.");
+    mysqli_set_charset($conexao,"utf8");
+
+    $consultaSQL = "SELECT tur.Nome, tur.Senha, tur.Id FROM Turma tur WHERE tur.Status='aberto'";
+    $resultado=mysqli_query($conexao, $consultaSQL) or die("Erro 404. Falha ao realizar a consulta SQL.");
+    ini_set ('default_charset', 'UTF8');
+
+    while($dados = mysqli_fetch_array($resultado)){
+        echo $dados['Nome'] . "<br>";
+        echo $dados['Senha'] . "<br>";
+        echo $dados['Id'] . "<br>";
+    }
+
+    mysqli_close($conexao);
+?>
